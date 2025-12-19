bd4f031b4195e0b3a43a89e7b420ff2a5209a20b
every-go <matteoevery498@gmail.com>
2025-12-19 18:02:12 +0100

    Modifica title, description e keywords

---
+++ src/php/animale.php
@@ -0,0 +1,10 @@
+// ATTENZIONE la pagina animale ha due segnaposti anche nel title, controlla
+// PS cancella questo commento quando hai completato
+
+<?php 
+    include "menu.php";
+    $content = file_get_contents("../html/animale.html");
+    $content = str_replace("[listaMenu]", $listaMenu, $content);
+    $content = str_replace("[listaFooter]", $listaFooter, $content);
+    echo $content;
+?>
