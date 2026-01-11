1e3f66021e914664a20cef199b2bfb452a5a6d31
Marco Brunello <markobrunello2010@gmail.com>
2026-01-11 12:44:03 +0100

    rifatto animazione notifica login via css

--- src/javascript/avviso.js
+++ src/javascript/avviso.js
@@ -1,25 +1,9 @@
-var durata = 4000; // 4 secondi
-// var intervallo = 333; // 3 aggiornamenti automatici per secondo
-var intervallo = 50; // aggiornamento ogni 50ms
-
-var avviso = document.getElementById('avviso');
-var progressBar = avviso ? avviso.querySelector('.progress-bar') : null;
-
-if (avviso && progressBar) {
-    var tempoPassato = 0;
-
-    var timer = setInterval(function () {
-        tempoPassato += intervallo;
-        var percentuale = 100 - (tempoPassato / durata) * 100;
-        progressBar.style.width = percentuale + '%';
-
-        if (tempoPassato >= durata) {
-            clearInterval(timer);
-
-            avviso.style.opacity = '0';
-            setTimeout(function () {
-                avviso.remove();
-            }, 500); // dissolvenza
-        }
-    }, intervallo);
-}
+setTimeout(function () {
+    var avviso = document.getElementById('avviso');
+    if (avviso) {
+        avviso.style.opacity = '0';
+        setTimeout(function () {
+            avviso.remove();
+        }, 500);
+    }
+}, 4000);
