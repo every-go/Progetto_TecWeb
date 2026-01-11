8b356b17d6f40f8ef873c3d7047ceb99a71fac5e
Marco Brunello <markobrunello2010@gmail.com>
2026-01-11 12:20:59 +0100

    fatto info login mo di notifica con progress bar discendente

--- src/index.php
+++ src/index.php
@@ -2,7 +2,9 @@
 include "menu.php";
 $messaggio = "";
 if (isset($_SESSION['messaggio_utente'])) {
-    $messaggio = "<div id='avviso' role='aler'> <p>" . $_SESSION['messaggio_utente'] . "</p> </div>";
+    $messaggio = "<div id='avviso' role='aler'> <p>" . $_SESSION['messaggio_utente'] . "</p> <div class='progress-container'>
+        <div class='progress-bar'></div>
+    </div> </div>";
     unset($_SESSION['messaggio_utente']);
 }
 $content = file_get_contents("html/index.html");
--- src/javascript/avviso.js
+++ src/javascript/avviso.js
@@ -1,9 +1,24 @@
-setTimeout(function() {
-    var avviso = document.getElementById('avviso');
-    if (avviso) {
-        avviso.style.opacity = '0';
-        setTimeout(function() { 
-            avviso.remove(); 
-        }, 500); // mezzo secondo per la dissolvenza
-    }
-}, 4000);
+var durata = 4000; // 4 secondi
+var intervallo = 333; // aggiornamento ogni 50ms
+
+var avviso = document.getElementById('avviso');
+var progressBar = avviso ? avviso.querySelector('.progress-bar') : null;
+
+if (avviso && progressBar) {
+    var tempoPassato = 0;
+
+    var timer = setInterval(function () {
+        tempoPassato += intervallo;
+        var percentuale = 100 - (tempoPassato / durata) * 100;
+        progressBar.style.width = percentuale + '%';
+
+        if (tempoPassato >= durata) {
+            clearInterval(timer);
+
+            avviso.style.opacity = '0';
+            setTimeout(function () {
+                avviso.remove();
+            }, 500); // dissolvenza
+        }
+    }, intervallo);
+}
